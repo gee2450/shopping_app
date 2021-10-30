194a4cf906eb1152ccc0d31e1949734e8e70cd64
윤민지 <gee2450@kookmin.ac.kr>
2021-10-31 04:20:24 +0900

    Login 에러 시 AlertDialog 생성 .

--- app/src/main/java/com/example/shoppingapp/LoginActivity.java
+++ app/src/main/java/com/example/shoppingapp/LoginActivity.java
@@ -1,15 +1,16 @@
 package com.example.shoppingapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
     private MySQLite mDBHelper;
@@ -28,6 +29,8 @@ public class LoginActivity extends AppCompatActivity {
     String phoneNum = "";
     String address = "";
 
+    String dialogMessage = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +40,9 @@ public class LoginActivity extends AppCompatActivity {
 
         idInput = (EditText) findViewById(R.id.id_input);
         pwInput = (EditText) findViewById(R.id.password_input);
+
+        // id, pw 불러오기
+        SetIdPw(savedInstanceState);
 
         btnSignUp = (Button) findViewById(R.id.btnSignUp);
         btnLogIn = (Button) findViewById(R.id.btnLogIn);
@@ -60,6 +66,9 @@ public class LoginActivity extends AppCompatActivity {
                     startActivity(intent);
                     finish();
                 }
+                else {
+                    MakeDialog("Login Error");
+                }
             }
         });
         btnMain.setOnClickListener(new View.OnClickListener() {
@@ -72,6 +81,15 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void SetIdPw(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            SharedPreferences pf = getSharedPreferences("user", MODE_PRIVATE);
+
+            idInput.setText(pf.getString("id", ""));
+            pwInput.setText(pf.getString("pw", ""));
+        }
+    }
+
     private Intent PutExtra(Intent intent) {
         intent.putExtra("isGuest", isGuest);
         intent.putExtra("id", id);
@@ -82,6 +100,15 @@ public class LoginActivity extends AppCompatActivity {
         return intent;
     }
 
+    private void MakeDialog(String title) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+        builder.setTitle(title);
+        builder.setMessage(dialogMessage);
+        builder.setPositiveButton("OK", null);
+        builder.create().show();
+    }
+
+
     private boolean logIn() {
         id = idInput.getText().toString();
 
@@ -89,7 +116,13 @@ public class LoginActivity extends AppCompatActivity {
         String[] projection = {"id", "password", "userName", "phoneNum", "address"};
         Cursor cur = db.query("userTable", projection, "id=?", new String[]{ id }, null, null, null);
 
-        if (cur == null) return false;
+        if (cur == null) {
+            return false;
+        }
+        else if (cur.getCount() < 1) {
+            dialogMessage = "해당 아이디를 찾을 수 없습니다. ";
+            return false;
+        }
 
         int password_col = cur.getColumnIndex("password");
         int userName_col = cur.getColumnIndex("userName");
@@ -106,11 +139,17 @@ public class LoginActivity extends AppCompatActivity {
         cur.close();
 
         if (!password.equals(pwInput.getText().toString())) {
-            Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+            dialogMessage = "올바르지 않은 비밀번호입니다. ";
             return false;
         }
 
         isGuest = false;
+
+        SharedPreferences pf = getSharedPreferences("user", MODE_PRIVATE);
+        SharedPreferences.Editor editor = pf.edit();
+        editor.putString("id", id);
+        editor.putString("pw", password);
+        editor.apply();
 
         return true;
     }
